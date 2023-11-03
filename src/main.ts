a58f38cb94cdc850c2f570bdff30cdbb91ae77d0
NiChrosia <nichrosian@gmail.com>
2023-11-03 18:57:42 -0500

    feat: add initial webgpu initialization

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,8 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+if (!navigator.gpu) throw new Error("WebGPU not supported!");
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+const adapter = await navigator.gpu.requestAdapter();
+if (!adapter) throw new Error("No GPU adapter found!");
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+const device = await adapter.requestDevice();
+
+export {};
