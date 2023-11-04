d211854cc56cadd10560b94c0e9cbb7ab2d42225
NiChrosia <nichrosian@gmail.com>
2023-11-03 19:32:57 -0500

    feat: add webgpu screen clearing
    feat: add mouse movement skeleton

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,39 @@
+function onMouseMove(e: MouseEvent) {
+	if (e.button != 1) return;
+
+	let position = [e.x, e.y];
+	let delta = [e.movementX, e.movementY];
+}
+
+const canvas = document.querySelector("canvas")!;
+
 if (!navigator.gpu) throw new Error("WebGPU not supported!");
 
 const adapter = await navigator.gpu.requestAdapter();
 if (!adapter) throw new Error("No GPU adapter found!");
 
 const device = await adapter.requestDevice();
+
+const context = canvas.getContext("webgpu")!;
+const canvasFormat = navigator.gpu.getPreferredCanvasFormat();
+
+context.configure({
+  device: device,
+  format: canvasFormat,
+});
+
+const encoder = device.createCommandEncoder();
+const pass = encoder.beginRenderPass({
+	colorAttachments: [{
+		view: context.getCurrentTexture().createView(),
+		loadOp: "clear",
+		clearValue: [0, 0.5, 0.7, 1],
+		storeOp: "store",
+	}]
+});
+
+pass.end()
+
+device.queue.submit([encoder.finish()]);
 
 export {};
